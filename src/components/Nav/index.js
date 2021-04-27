e4ec5985709f0cd6177ff358d677df02ac1d5a4e
w.hunt <wfh2d88@gmail.com>
2021-04-27 18:57:22 -0500

    20.3.1

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -27,7 +27,7 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
             {" "}
             📸
@@ -38,7 +38,7 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
               About me
             </a>
           </li>
